b2f573b4573c57ee2aa18b507d39787af73fb78e
andychicken <83952965+andychicken@users.noreply.github.com>
2021-07-20 13:26:38 +0700

    fix some errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import {
 } from '@nestjs/common';
 
 import {
+  MongooseModule,
+} from '@nestjs/mongoose';
+
+import {
   UserModule,
 } from '../user/user.module';
 
+import {
+  AppController,
+} from './app.controller';
+
+import {
+  AppService,
+} from './app.service';
+
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, MongooseModule.forRoot('mongodb://127.0.0.1:27017/database')],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,29 +1,28 @@
 import {
-    Module,
-  } from '@nestjs/common';
-  
-  import {
-    MongooseModule,
-  } from '@nestjs/mongoose';
-  
-  import {
-    UserController,
-  } from './user.controller';
-  
-  import {
-    UserService,
-  } from './user.service';
-  
-  import {
-    UserSchema,
-  } from './schemas/schema.user';
-  
-  @Module({
-    imports: [MongooseModule.forFeature([{
-      name: 'User', schema: UserSchema,
-    }])],
-    controllers: [UserController],
-    providers: [UserService],
-  })
-  export class UserModule {}
-  
+  Module,
+} from '@nestjs/common';
+
+import {
+  MongooseModule,
+} from '@nestjs/mongoose';
+
+import {
+  UserController,
+} from './user.controller';
+
+import {
+  UserService,
+} from './user.service';
+
+import {
+  UserSchema,
+} from './schemas/schema.user';
+
+@Module({
+  imports: [MongooseModule.forFeature([{
+    name: 'User', schema: UserSchema,
+  }])],
+  controllers: [UserController],
+  providers: [UserService],
+})
+export class UserModule {}
